c7291818edcd622e38dc0371be470d8bf23a18b5
Elanas <elaasinfo@gmail.com>
2018-01-30 15:11:27 +0200

    3cias darbas

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("Iveskite du skaicius");
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("Suma: " + (a+b));
+        System.out.println("Skirtumas: " + (a-b));
+    }
+}
